359f0eded27715e06f4fc896320268252b2b958c
WackyChalck77 <vsevolod.evtukhov@gmail.com>
2025-06-07 02:24:41 +0300

    update on ex-es

--- 11/main.go
+++ 11/main.go
@@ -4,25 +4,27 @@ import (
 	"fmt"
 )
 
-type impl struct{}
+type impl struct{} //создается тип данных impl
 
-type I interface {
+type I interface { //создается тип данных интерфейс
 	C()
 }
 
-func (*impl) C() {}
+func (*impl) C() {} //создается метод на стракт,
+//возвращает С
 
-func A() I {
+func A() I { //функция A, ничего не берет, возвращает I-интерфейс
 	return nil
 }
 
-func B() I {
-	var ret *impl
-	return ret
+func B() I { //функция B, ничего не берет, возвращает I-интерфейс
+	var ret *impl //переменная-указатель ret на структуру impl
+	return ret    //возвращает указание на impl
 }
 
 func main() {
-	a := A()
-	b := B()
-	fmt.Println(a == b)
+	a := A() // объявление переменной, ф-ция возвращает nil
+	b := B() // приравнивается указателю на impl-структуру,
+	//это не nil
+	fmt.Println(a == b) //выдаст false (nil != *impl)
 }
